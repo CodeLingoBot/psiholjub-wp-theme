a9e1e343df5e1207d25d3c947223c994065445a1
Damjan Pavlica <mudroljub@gmail.com>
2017-02-26 22:38:05 +0100

    razvoj skolakoda

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
   <?php mh_before_footer(); ?>
   <footer class="mh-copyright-wrap">
   	<div class="mh-container mh-container-inner clearfix">
-  		<p class="mh-copyright">Psiholjub &copy; | Jer on zna odgovore na sva pitanja tvoje duše | <span class="float-right">Izrada i održavanje: <a href="https://github.com/mudroljub">Damjan Pavlica</a></span></p>
+  		<p class="mh-copyright">Psiholjub &nbsp;Ψ<span class="narandzasta-slova srce">♥</span>&nbsp; Jer on zna odgovore na sva pitanja tvoje duše | <span class="float-right">Razvoj: <a href="http://skolakoda.org/">Škola koda</a></span></p>
   	</div>
   </footer>
   <?php mh_after_footer(); ?>
